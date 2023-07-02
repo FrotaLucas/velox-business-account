f0fe6b505453da08c96703de8515204c2822d95b
FrotaLucas <lucasdiasfrota@gmail.com>
2023-07-02 23:46:32 +0200

    build line separation between questions

--- accountopen.js
+++ accountopen.js
@@ -1,5 +1,32 @@
 window.addEventListener("DOMContentLoaded", function() {
   var contentClass = document.getElementsByClassName("collapsible");
+  var cardToggle = document.getElementsByClassName("open-benefit");
+  var card = document.getElementsByClassName("hidden-benefit");
+
+
+
+  for (var i = 0; i < cardToggle.length; i++) {
+        cardToggle[i].addEventListener("click", function() {
+        var img = this.querySelector("img");
+
+        var index = Array.from(cardToggle).indexOf(this);
+        var computedStyle = window.getComputedStyle(card[index]);
+        var display = computedStyle.getPropertyValue("display");
+  
+
+        if(display === "none")
+        {
+          card[index].style.display = "block";
+          img.src = "./svgs/arrow_up.svg"
+        }
+
+        else {
+          card[index].style.display = "none";
+          img.src = "./svgs/arrow_down.svg"
+
+        }
+      })
+  }
 
   for (var i = 0; i < contentClass.length; i++) {
     contentClass[i].addEventListener("click", function() {
@@ -20,4 +47,7 @@ window.addEventListener("DOMContentLoaded", function() {
       }
     });
   }
+
+
+
 });
